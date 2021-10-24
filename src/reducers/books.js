4d2c588e32057ea05ea05e6ff544c14308d85421
rloterh <rloterh@gmail.com>
2021-10-24 20:05:00 +0000

    Add sample books to list

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,7 +1,25 @@
-const bookReducer = (state = [], action) => {
+const initialState = [
+  {
+    id: Math.random(),
+    title: 'React Fundamentals',
+    category: 'Learning - Beginner',
+  },
+  {
+    id: Math.random(),
+    title: 'React for Developers',
+    category: 'Learning - Intermediate',
+  },
+  {
+    id: Math.random(),
+    title: 'Reacr for Professionals',
+    category: 'Learning - Advanced',
+  },
+];
+
+const bookReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'CREATE_BOOK':
-      return [...state, action.book];
+      return [...state, action.payload];
     case 'REMOVE_BOOK':
       return state.filter((book) => book !== action.payload);
     default:
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
-import { combineReducers } from "redux";
-import bookReducer from "./books";
+import { combineReducers } from 'redux';
+import bookReducer from './books';
 
-const rootReducer = combineReducers({ bookReducer });
-export default rootReducer;
+const reducer = combineReducers({ bookReducer });
+
+export default reducer;
