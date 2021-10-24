6d718b2ca134890fcd6d82815c08d3d3b7ea1b28
rloterh <rloterh@gmail.com>
2021-10-24 20:11:19 +0000

    Render App component in a Provider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import bookReducer from './reducers/books';
+
+const store = createStore(bookReducer);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root'),
 );
 
