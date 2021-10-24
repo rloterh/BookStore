1f89f485aefb42f24e9538b102461369f9977d93
rloterh <rloterh@gmail.com>
2021-10-24 19:44:52 +0000

    Create and connect BooksList

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,5 +1,31 @@
-const BooksList = () => {
+import { useSelector } from 'react-redux';
+import Book from '../components/Books';
 
+const BooksList = () => {
+  const books = useSelector((state) => state);
+  return (
+    <div>
+      <h2>Book List</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Book ID</th>
+            <th>Title</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            books.map((book) => (
+              <tr key={book.id}>
+                <Book book={book} />
+              </tr>
+            ))
+           }
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default BooksList;
