8600204da95dd18ad650d7c64e0d0cf91d94b025
rloterh <rloterh@gmail.com>
2021-10-24 16:11:29 +0000

    Implement Book component

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,5 +1,19 @@
-const Book = () => {
+import PropTypes from "prop-types";
 
-};
+const Book = ({ book }) => (
+    <>
+      <td>{book.id}</td>
+      <td>{book.title}</td>
+      <td>{book.category}</td>
+    </>
+  );
+
+  Book.propTypes = {
+    book: PropTypes.shape({
+      id: PropTypes.number,
+      title: PropTypes.string,
+      category: PropTypes.string,
+    }),
+  };
 
 export default Book;
