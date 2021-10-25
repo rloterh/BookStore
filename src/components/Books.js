cc40a080133ca06aa5298a4272d1420c3d291510
rloterh <rloterh@gmail.com>
2021-10-25 19:57:22 +0000

    Add 'remove book' column to table

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,15 +1,21 @@
 import PropTypes from 'prop-types';
 
-const Book = ({ book }) => (
+const Book = ({ book, handleRemoveBook }) => (
   <>
     <td>{book.id}</td>
     <td>{book.title}</td>
     <td>{book.category}</td>
+    <td>
+      <button type="button" onClick={() => handleRemoveBook(book)}>
+        Delete
+      </button>
+    </td>
   </>
 );
 
 Book.defaultProps = {
   book: {},
+  handleRemoveBook: () => {},
 };
 
 Book.propTypes = {
@@ -18,6 +24,7 @@ Book.propTypes = {
     title: PropTypes.string,
     category: PropTypes.string,
   }),
+  handleRemoveBook: PropTypes.func,
 };
 
 export default Book;
