00506984aa14cf6708bc86b9fe7ec1af2b658986
rloterh <rloterh@gmail.com>
2021-10-26 10:42:57 +0000

    Add filter reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import bookReducer from './books';
+import filterBookReducer from './filter';
 
-const reducer = combineReducers({ bookReducer });
+const reducer = combineReducers({ bookReducer, filterBookReducer });
 
 export default reducer;
